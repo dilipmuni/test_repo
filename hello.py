88249d75f23a0d9a5b94782f1766e621effe3abb
unknown <dilipmuni12@gmail.com>
2021-06-03 20:24:50 +0530

    fries

--- hello.py
+++ hello.py
@@ -2,5 +2,9 @@ print("hello world")
 hungry = input("are you hungry")
 if hungry== "yes":
     print("eat somosa")
+    print('eat Pizza')
+    print('eat burger')
+    print('eat fries')
+
 else : 
     print("do your homework")
