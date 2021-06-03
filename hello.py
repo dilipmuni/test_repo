775859244f62f634821709e4a8bc7951d2b94f8e
unknown <dilipmuni12@gmail.com>
2021-06-03 20:20:38 +0530

    thirsty

--- hello.py
+++ hello.py
@@ -3,4 +3,7 @@ hungry = input("are you hungry")
 if hungry== "yes":
     print("eat somosa")
 else : 
-    print("do your homework")
+    thirsty=input("are you thirsty : ")
+    if thirsty=="yes":
+            print("Drink Water")
+    //print("do your homework")
